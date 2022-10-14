00864e558420daf2a8f83dd2eba61aa9f7a5da5e
Emily Evett <eevett7@gmail.com>
2022-10-14 16:46:23 -0600

    delete

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#navigate into our working directory where we have all our github file 
+#navigate into our working directory where we have all our github files 
 cd /home/ec2-user/express-app
 
 #add npm and node to path 
