c95d962e86ecd0c5a1977186ef382344c30de84c
Emily Evett <eevett7@gmail.com>
2022-10-14 18:07:26 -0600

    test

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ const app = express();
 
 let path = require("path");
 
+app.get('/', (req, res) => {
+    res.send("welcome");
+})
+// app.set("view engine", "ejs");
+// app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+// const knex = require("knex")({
+//     client: "mysql",
+//     connection: {
+//         host: "santa-database-instance-1.cmvut5jksx6g.us-east-2.rds.amazonaws.com",
+//         user: "admin",
+//         password: "adminadmin",
+//         database: "SantaDonuts",
+//         port: 3306
+//     }
+// });
 
-const knex = require("knex")({
-    client: "mysql",
-    connection: {
-        host: "santa-database-instance-1.cmvut5jksx6g.us-east-2.rds.amazonaws.com",
-        user: "admin",
-        password: "adminadmin",
-        database: "SantaDonuts",
-        port: 3306
-    }
-});
-
-app.get("/", (req, res) => {
-    knex.select().from("Donuts").then(Donuts => {
-        res.render("displaydonuts", { mydonuts: Donuts });
-    });
-});
+// app.get("/", (req, res) => {
+//     knex.select().from("Donuts").then(Donuts => {
+//         res.render("displaydonuts", { mydonuts: Donuts });
+//     });
+// });
 
 app.listen(3000, () => console.log("Express App has started and server is listening!"));
 
-app.get('/', )
+// app.get('/', )
